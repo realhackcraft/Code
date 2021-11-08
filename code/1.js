4a357f87fff016ab32fac72e80b73e9d743a2032
realhackcraft <65378120+realhackcraft@users.noreply.github.com>
2021-11-08 15:50:32 -0500

    Just stop reading these for a while, ok.. (real commit: made uselful teaks to the code)

--- code/1.js
+++ code/1.js
@@ -11,12 +11,36 @@ function Timer () {
 }
 
 function KeyDownHandler (Event) {
-  if (Event.keyCode == 37 && x_pos > 0) x_pos = x_pos - 10;   //left
-  if (Event.keyCode == 38 && y_pos > 0) {y_pos = y_pos - 10;}   //up
-  if (Event.keyCode == 39 && x_pos + Img.width < game.width) {x_pos = x_pos + 10;}   //right
-  if (Event.keyCode == 40 && y_pos + Img.height < game.height) {y_pos = y_pos + 10;}   //down
+  if (Event.keyCode == 37) left();   //left
+  if (Event.keyCode == 38) up();    //up
+  if (Event.keyCode == 39) right();   //right
+  if (Event.keyCode == 40) down;   //down
   Event.preventDefault()
 }
 
-setInterval(Timer, 40);
+function left() {
+  if (x_pos > 0) {
+  x_pos = x_pos - 10;
+  }
+}
+
+function up() {
+  if (y_pos > 0) {
+  y_pos = y_pos - 10;
+  }
+}
+
+function right() {
+  if (x_pos + Img.width < game.width) {
+  x_pos = x_pos + 10;
+  }
+}
+
+function down() {
+  if (y_pos + Img.height < game.height) {
+    y_pos = y_pos + 10;
+  }
+}
+
+setInterval(Timer, 1);
 addEventListener("keydown", KeyDownHandler);
